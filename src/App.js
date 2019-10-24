77779d770230d8a30bcf89c17f0ed7204c654506
Rodrigo Nogueira <rdg6design@gmail.com>
2019-10-24 11:20:53 -0300

    react-native whatsapp clone

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import FormLogin from './components/FormLogin'
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+    <FormLogin />
+    );
+  }
+}
