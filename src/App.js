caeb5882c8f734686033a7429c5ce6282e33fcea
Rodrigo Nogueira <rdg6design@gmail.com>
2019-10-25 09:34:12 -0300

    Criando o componente FormCadastro

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import FormLogin from './components/FormLogin'
+import FormCadastro from './components/FormCadastro'
+
 export default class HelloWorldApp extends Component {
   render() {
     return (
-    <FormLogin />
+    <FormCadastro />
     );
   }
 }
